aee3e6be21b9d7e281f56b07805a7f5abe63109c
josexy <josephxrays@163.com>
2021-12-11 00:27:50 +0800

    update

--- example.go
+++ example.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/josexy/gopool/pool"
 	"runtime"
 	"sync/atomic"
+	"time"
+
+	"github.com/josexy/gopool/pool"
 )
 
 var cnt int64
@@ -16,12 +18,38 @@ func main() {
 	defer func() { fmt.Println(cnt) }()
 	defer p.Shutdown()
 
+	arg := struct {
+		name string
+		age  int
+	}{
+		"mike", 12,
+	}
+
 	for i := 0; i < runtime.NumCPU(); i++ {
-		p.Submit(func() interface{} {
+		p.Submit(func(i ...interface{}) interface{} {
+			fmt.Println("==> ", i)
 			for j := 0; j < SIZE; j++ {
 				atomic.AddInt64(&cnt, 1)
 			}
 			return nil
-		})
+		}, arg, "hello world")
+	}
+
+	var rws []*pool.ResultWorker
+	for i := 0; i < runtime.NumCPU(); i++ {
+		resW := p.SubmitResult(func(i ...interface{}) interface{} {
+			fmt.Println("==> ", i)
+			time.Sleep(time.Second)
+			return 10000
+		}, time.Second*2, arg, 1000, "hello world")
+
+		rws = append(rws, resW)
+	}
+
+	fmt.Println("get result")
+	for _, rw := range rws {
+		go func(x *pool.ResultWorker) {
+			fmt.Println("result: ", x.Get())
+		}(rw)
 	}
 }
